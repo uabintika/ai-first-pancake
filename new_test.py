8d92d5628ffaca507fff4ba88f614fb300d8e1a6
uabintika <uab.intika@gmail.com>
2025-05-16 20:09:35 +0300

    2025 05 16

---
+++ new_test.py
@@ -0,0 +1,18 @@
+person_list = []
+
+while True:
+
+person_name = input("Enter person's name: ")
+person_age = input("Enter person's age: ")
+person_city = input("Enter person's city: ")
+
+person = {
+    'name' : person_name,
+    'age' : person_age,
+    'city' : person_city
+}
+
+person_list.append(person)
+
+for p in person_list:
+    print(f"Name: {p['name']}, Age: {p['age']}, City: {p['city']}")
---
+++ python_concepts.py
@@ -0,0 +1,31 @@
+a = 10
+b = 'hello'
+c = 3.14
+d = True
+
+
+
+console_input = input('Enter your name: ')
+print('Hello, ' +console_input)
+
+if a > 5:
+    print('a is greater than 5')
+elif a == 5:
+    print('a is euqal to 5')    
+else:
+    print ('a is less than 5')
+
+for i in range(5):
+    print('Interation:', i)
+    print('Interation:', i)
+    print('Interation:', i)
+
+while i < 3:
+    print('While loop iteration:', 1)
+    i += 1
+    
+    datetime = __import__('datetime')
+    current_date = datetime.now()
+
+    birth_date = datetime(1995, 4, 1)
+
