434bc5c54d447604c8f0d28804e540cb88d24a96
uabintika <uab.intika@gmail.com>
2025-05-20 19:59:14 +0300

    2025 05 20

---
+++ openai_t_w.py
@@ -0,0 +1,50 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
+
+token = os.getenv("AIKEY")
+endpoint = "https://models.github.ai/inference"
+model = "openai/gpt-4.1-nano"
+
+client = OpenAI(
+    base_url=endpoint,
+    api_key=token,
+)
+
+print("Ask me anything, type 'exit' to leave")
+
+while True:
+    try:
+        question = input("Ask me anything: ")
+        if question.lower() == "exit":
+            print("Exiting...")
+            break
+
+        response = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "system",
+                    "content": "Atsakyk į klausimus visus užduodamus lietuviškai, nepriklauso kokia kalba užduotas klausimas.",
+                },
+                {
+                    "role": "user",
+                    "content": question,
+                }
+            ],
+            temperature=1.0,
+            top_p=1.0,
+            model=model
+        )
+
+        for r in response.choices:
+            print("Atsakymas: ", r.message.content)
+        
+        with open("output.txt", "a", encoding="utf-8") as file:
+            file.write("Klausimas: " + question + "\n")
+            file.write(r.message.content + "\n")
+
+    except Exception as e:
+        print("Error: ", e)
+        break
+
